181d489b56906983de637603ff2fce65654d27b4
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-09-17 15:41:20 -0700

    Add files via upload

--- platforms/maps/Level2.gd
+++ platforms/maps/Level2.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 func _process(delta: float) -> void:
+	# 13 is what the count equals currently. This is not the way to go about it.
 	if get_tree().get_node_count() == 13:
 		queue_free()
--- platforms/nodes/Solidemy.gd
+++ platforms/nodes/Solidemy.gd
@@ -10,6 +10,6 @@ func _on_Area2D_body_exited(body: KinematicBody2D) -> void:
 	if body != null:
 		player_inside = false
 	
-func _process(delta):
+func _process(delta: float) -> void:
 	if player_inside and Input.is_action_pressed("attack"):
 		queue_free()
